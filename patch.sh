ca9dbc5803bb6f4e4c7e55762a178dba198ca3b4
Jian Li <jianl@redhat.com>
2023-06-20 14:32:05 +0800

    Update patch.json

--- patch.sh
+++ patch.sh
@@ -1,1 +1,8 @@
 oc patch clusterversion version --type json -p '[{"op": "add", "path": "/spec/channel", "value": "stable-4.13"}, {"op": "add", "path": "/spec/upstream", "value": "https://raw.githubusercontent.com/JianLi-RH/ota/main/patch.json"}]'
+
+
+oc patch clusterversion version --type=merge -p '{"spec": {"overrides":[{"kind": "Deployment", "name": "network-operator", "namespace": "openshift-network-operator", "unmanaged": true, "group": "apps"}]}}'
+oc patch clusterversion version --type=json -p '[{"op": "remove","path":"/spec/overrides"}]'
+
+
+oc -n openshift-config-managed patch configmap admin-gates --type=json -p='[{"op": "remove", "path": "/data/ack-4.8-dummy"}]'

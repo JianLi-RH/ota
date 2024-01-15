27c8049e40b0fc7160ff5592da5f740ebc4c9415
Jian Li <jianl@redhat.com>
2024-01-15 10:50:54 +0800

    docker push image

--- ack.sh
+++ ack.sh
@@ -1,9 +1,9 @@
 # ack
-[root@localhost ~]# oc411 -n openshift-config-managed get configmap admin-gates -o json | jq -r '.data|keys[]'
+[root@localhost ~]# oc414 -n openshift-config-managed get configmap admin-gates -o json | jq -r '.data|keys[]'
 ack-4.11-kube-1.25-api-removals-in-4.12
 [root@localhost ~]# 
 
-oc413 -n openshift-config patch cm admin-acks --patch '{"data":{"ack-4.13-kube-1.27-api-removals-in-4.14":"true"}}' --type=merge
+oc414 -n openshift-config patch cm admin-acks --patch '{"data":{"ack-4.13-kube-1.27-api-removals-in-4.14":"true"}}' --type=merge
 
 oc413 -n openshift-config get cm admin-acks
 
--- osus.sh
+++ osus.sh
@@ -1,34 +1,66 @@
 https://docs.openshift.com/container-platform/4.13/updating/updating-restricted-network-cluster/restricted-network-update-osus.html#update-service-install-cli_updating-restricted-network-cluster-osus
 
+# graph-data
+quay.io/openshifttest/graph-data:latest
 
+
+###############################################################################################################
+######################################## 在普通环境安装OSUS Operator: ###########################################
+###############################################################################################################
+https://polarion.engineering.redhat.com/polarion/#/project/OSE/workitem?id=OCP-35869
+
+###############################################################################################################
+
+
+###############################################################################################################
+################################ 在disconnected环境安装OSUS Operator: ###########################################
+###############################################################################################################
+https://docs.openshift.com/container-platform/4.14/updating/updating_a_cluster/updating_disconnected_cluster/disconnected-update-osus.html#update-service-overview_updating-restricted-network-cluster-osus
+
+https://polarion.engineering.redhat.com/polarion/#/project/OSE/workitem?id=OCP-40819
+ 1. Disable the default catalog source
+# ./oc patch OperatorHub cluster --type json -p '[{"op": "add", "path": "/spec/disableAllDefaultSources", "value": true}]'
+
+
+https://polarion.engineering.redhat.com/polarion/#/project/OSE/workitem?id=OCP-43326
+Mirror graph-data container image to disk then to local registry by using oc
+
+# cat ./Dockerfile
+FROM registry.access.redhat.com/ubi8/ubi:8.1
+
+RUN curl -L -o cincinnati-graph-data.tar.gz https://api.openshift.com/api/upgrades_info/graph-data
+
+RUN mkdir -p /var/lib/cincinnati-graph-data && tar xvzf cincinnati-graph-data.tar.gz -C /var/lib/cincinnati-graph-data/ --no-overwrite-dir --no-same-owner
+
+CMD ["/bin/bash", "-c" ,"exec cp -rp /var/lib/cincinnati-graph-data/* /var/lib/cincinnati/graph-data"]
+
+# podman build -f Dockerfile -t ${LOCAL_REGISTRY}/rh-osbs/cincinnati-graph-data-container:v5.0.0
+# podman push ${LOCAL_REGISTRY}/rh-osbs/cincinnati-graph-data-container:v5.0.0
+###############################################################################################################
+
+
+
+###############################################################################################################
+############################################# Install OSUS by cli #############################################
+###############################################################################################################
+https://docs.openshift.com/container-platform/4.10/updating/updating-restricted-network-cluster/restricted-network-update-osus.html
+https://polarion.engineering.redhat.com/polarion/#/project/OSE/workitems/testcase?query=OCP-35944
+
+# OSUS Service URL:
+oc get updateservice sample -n   -o jsonpath='{.status.policyEngineURI}/api/upgrades_info/v1/graph{"\n"}'
 
 1. Build and mirror graph-data container image to local registry as Non-Root User
 # podman build -f ./Dockerfile -t ${DIS_REGISTRY}/rh-osbs/cincinnati-graph-data-container:v4.6.0
 # podman push ${DIS_REGISTRY}/rh-osbs/cincinnati-graph-data-container:v4.6.0
 
-
+# 查看已安装的CatalogSource
 oc get catalogsource -n openshift-marketplace
+# qe-app-registry
 
-
-# Create UpdateService resouce
-# cat cincy.yaml 
-apiVersion: cincinnati.openshift.io/v1beta1
-kind: UpdateService
-metadata:
-  name: my-cincy
-  namespace: osus
-spec:
-  replicas: 1
-  releases: "quay.io/openshift-release-dev/ocp-release"
-  graphDataImage: "quay.io/openshifttest/graph-data:v5.0.0"
-
-# oc411 create -f cincy.yaml 
-cincinnati.cincinnati.openshift.io/my-cincy created
-
-
-
+1. 创建namaspace
 oc create ns osus
 
+2. 创建OperatorGroup
 cat <<EOF > og.yaml 
 apiVersion: operators.coreos.com/v1
 kind: OperatorGroup
@@ -42,6 +74,7 @@ EOF
 
 oc create -f og.yaml 
 
+3. 创建Subscription
 cat <<EOF > sub.yaml 
 apiVersion: operators.coreos.com/v1alpha1
 kind: Subscription
@@ -72,12 +105,28 @@ spec:
   sourceNamespace: "openshift-marketplace"
   name: "cincinnati-operator"
 
+# Create UpdateService resouce
+# cat cincy.yaml 
+apiVersion: cincinnati.openshift.io/v1beta1
+kind: UpdateService
+metadata:
+  name: my-cincy
+  namespace: osus
+spec:
+  replicas: 1
+  releases: "quay.io/openshift-release-dev/ocp-release"
+  graphDataImage: "quay.io/openshifttest/graph-data:v5.0.0"
+
+# oc411 create -f cincy.yaml 
+cincinnati.cincinnati.openshift.io/my-cincy created
+
 
 # oc delete sub osus-sub -n osus
 # oc delete csv update-service-operator.v4.6.0 -n osus
 # oc delete og osus-og -n osus
 
 
+# 检查 OSUS Service URL:
 oc get route my-cincy-policy-engine-route -o jsonpath='{.spec.host}' -n osus
 curl -skH 'Accept:application/json' "https://$url/api/upgrades_info/v1/graph?channel=stable-4.10" -o /dev/null -w "status: %{http_code}\n"
 
@@ -100,8 +149,7 @@ curl -skH 'Accept:application/json' "https://$url/api/upgrades_info/v1/graph?cha
 }
 
 
-# graph-data
-https://quay.io/openshifttest/graph-data:latest
+
 
 
 # Disconnected 
@@ -131,32 +179,43 @@ oc patch proxy cluster --type json -p '[{"op": "add", "path": "/spec/trustedCA/n
 oc adm release mirror -a config.json --from=quay.io/openshift-release-dev/ocp-release:4.11.28-x86_64 \
   --to=upshift.mirror-registry.qe.devcluster.openshift.com:5000/openshift-release-dev/ocp-release \
   --to-release-image=upshift.mirror-registry.qe.devcluster.openshift.com:5000/ocp-release:4.11.28-x86_64
-2
+
+
+
+
+# 出现Auth错误的时候，是secret没有配置，或者配置的不正确：
 #手动改cluster 的pull secret
 oc extract secret/pull-secret -n openshift-config --confirm
-#.dockerconfigjson is generated, add auth of your registry to the file
+#.dockerconfigjson is generated, add auth of your registry to the file， upshift 的用户名密码是dummy:dummy，需要base64加密
 oc set data secret/pull-secret -n openshift-config --from-file=.dockerconfigjson=.dockerconfigjson
 
-# 创建trusted_ca.yaml
-使用： https://gitlab.cee.redhat.com/aosqe/flexy-templates/-/blob/master/functionality-testing/certs/all-in-one.9/client_ca.crt
 
-cert_of_local_registry=$(cat client_ca.crt)
+###############################################################################################################
+################################################ 创建trusted_ca ################################################
+###############################################################################################################
+# quay.io的image不需要证书就能访问
 
-cat <<EOF >trusted_ca.yaml
+证书文件： https://gitlab.cee.redhat.com/aosqe/flexy-templates/-/blob/master/functionality-testing/certs/all-in-one.9/client_ca.crt
+
+# 方法1 （参考： https://polarion.engineering.redhat.com/polarion/#/project/OSE/workitem?id=OCP-42669）
+oc -n openshift-config create cm trusted-ca --from-file=upshift.mirror-registry.qe.devcluster.openshift.com..5000=./client_ca.crt --from-file=updateservice-registry=./client_ca.crt
+
+# 方法2
+cat <<EOF | oc -n openshift-config create -f -
 apiVersion: v1
 kind: ConfigMap
 metadata:
   name: trusted-ca
 data:
   updateservice-registry: |
-    $cert_of_local_registry
-  upshift.mirror-registry.qe.devcluster.openshift.com.5000: |
-    $cert_of_local_registry
+    -----BEGIN CERTIFICATE-----
+    -----END CERTIFICATE-----
 EOF
 
-oc -n openshift-config create -f  trusted_ca.yaml
+# 获取当前的ca
+oc get cm -n openshift-config trusted-ca -oyaml
 oc patch image.config.openshift.io cluster -p '{"spec":{"additionalTrustedCA":{"name":"trusted-ca"}}}' --type merge
-
-
-
-https://gitlab.cee.redhat.com/aosqe/flexy-templates/-/blob/master/functionality-testing/certs/all-in-one.9/client_ca.crt
+# 检查patch结果：
+oc get  image.config.openshift.io cluster -o json
+# 删除additionalTrustedCA
+oc patch image.config.openshift.io/cluster --type=json -p '[{"op": "remove","path":"/spec/additionalTrustedCA"}]'
